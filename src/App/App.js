89679fb066436d62a4eadf3a4fe4f4117c8d48fe
pedrocopes <petercopes.96@gmail.com>
2021-08-10 10:13:44 -0300

    lazy loading added.

--- src/App/App.js
+++ src/App/App.js
@@ -1,36 +1,35 @@
-import React from "react";
+import React, { Suspense } from "react";
 import "./App.css";
 import { Switch, Route, Redirect } from "react-router-dom";
-import Home from "../screens/Home/Home";
-import AddHero from "../screens/AddHero/AddHero";
-import HeroDetails from "../screens/HeroDetails/HeroDetails";
 import "bootstrap/dist/css/bootstrap.min.css";
 import Login from "../components/Login/Login";
-
 import NavBar from "../components/NavBar/NavBar";
-
+const Home = React.lazy(() =>   import('../screens/Home/Home'));
+const AddHero = React.lazy(() =>  import("../screens/AddHero/AddHero"));
+const HeroDetails = React.lazy(() =>  import("../screens/HeroDetails/HeroDetails"));
 function App() {
   return (
     <>
       <div className="App overflow-hidden">
         <Login />
         <NavBar />
-        <Switch>
-          <Route path="/home">
-            <Home />
-          </Route>
-          <Route path="/add-hero">
-            <AddHero />
-          </Route>
-          <Route path="/hero-details/:id">
-            <HeroDetails />
-          </Route>
-          <Route path="/">
-            <Redirect to="/home" exact />
-          </Route>
-        </Switch>
+        <Suspense fallback={<p></p>}>
+          <Switch>
+            <Route path="/home">
+              <Home />
+            </Route>
+            <Route path="/add-hero">
+              <AddHero />
+            </Route>
+            <Route path="/hero-details/:id">
+              <HeroDetails />
+            </Route>
+            <Route path="/">
+              <Redirect to="/home" exact />
+            </Route>
+          </Switch>
+        </Suspense>
       </div>
-      
     </>
   );
 }
